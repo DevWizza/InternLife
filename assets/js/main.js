96eecb08ee115a9031b7895f94d0903ca35ac586
Luis <luis.pena.nunez.developer@gmail.com>
2017-02-25 09:32:16 -0600

    Introduced Post Layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,4 +4,50 @@ $(document).ready(function() {
 		$(this).toggleClass("menu-trigger--open");
 		$(".menu").toggleClass("menu--open");
 	});
+
+
+	// Header will have its color in scroll
+	$(window).scroll(function(){
+		headerTransparent();
+	});
+
+	var mainHeader = $(".header");
+
+	function headerTransparent() {
+		var offset = 200;
+
+		if( $(window).scrollTop() > offset && mainHeader.hasClass('header--transparent')) {
+			mainHeader.removeClass('header--transparent');
+			mainHeader.addClass('header--with-colors');
+
+		} else if ( $(window).scrollTop() <= offset && mainHeader.hasClass('header--with-colors')) {
+			mainHeader.addClass('header--transparent');
+			mainHeader.removeClass('header--with-colors');
+		}
+   }
+
+
+	// Detect the screen size, and change the background for Post
+	var minWidthSmall = window.matchMedia('(min-width: 768px)');
+
+	var postBgMobile = $('[data-post-bg-mobile]');
+	var postBgDesktop = $('[data-post-bg-desktop]');
+
+	var elementBackground = $('[data-bg-image]');
+
+	if (minWidthSmall.matches) {
+		callImages(postBgDesktop, 'data-post-bg-desktop');
+
+	} else {
+		callImages(postBgMobile, 'data-post-bg-mobile');
+	}
+
+	function callImages(dataAttr, dataAttrContent) {
+		for (var i = 0; i < dataAttr.length; i++) {
+			var url = dataAttr[i].getAttribute(dataAttrContent);
+			dataAttr[i].style.backgroundImage='url("' + url + '")';
+		}
+	}
+
+	callImages(elementBackground, 'data-bg-image');
 });
